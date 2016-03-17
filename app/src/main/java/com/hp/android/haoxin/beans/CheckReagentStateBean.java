87316288e9af6d55ed4def71b37da63b2eedac92
AZZ <806622085@qq.com>
2016-03-17 10:48:15 +0800

    试剂判断跳过某试剂后就不再判断该试剂

--- app/src/main/java/com/hp/android/haoxin/beans/CheckReagentStateBean.java
+++ app/src/main/java/com/hp/android/haoxin/beans/CheckReagentStateBean.java
@@ -47,8 +47,6 @@ public class CheckReagentStateBean {
         }
         //逐个检查ABCDE试剂是否充足
         for (int i = index; i < hasCheckedReagent.length; i++) {
-
-//            index++; //指针自增1
 //
 //            if (hasCheckedReagent[i]) { //如果当前试剂已经检查过，就跳到下一试剂
 //                Log.d("CheckReagentStateBean", "当前试剂（"+getReagentString(i)+"）已经检查过，检查下一试剂");
@@ -59,6 +57,7 @@ public class CheckReagentStateBean {
 
             if (Global.getSystemStateBean().isReagentEnough(i)) {
                 Log.i("CheckReagentStateBean", getReagentString(i) + "试剂充足");
+                index++; //指针自增1
                 continue;
             } else {
                 setErrorReagent(i);
